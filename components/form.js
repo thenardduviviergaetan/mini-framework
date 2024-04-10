44e99b0c2479a5d74fadbe3b0978221f71cde07a
GMZZ <legaynicolas76@gmail.com>
2024-04-10 15:32:58 +0200

    form done

--- components/form.js
+++ components/form.js
@@ -1,24 +1,8 @@
 import Component from "./component.js";
-import Div from "./div.js";
 
 export default class Form extends Component{
     constructor(){
         super("form")
-    }
-    newFormElement(element,label){
-        let divElement = new Div();
-        divElement.appendChild(label);
-        divElement.appendChild(element);
-        this.appendChild(divElement);
-    }
-    newTextinput(id,label,...className){
-
-    }
-    newCheckbox(id,label,value,...className){
-        
-    }
-    newRadioSelecteur(id,label,tabValue,...className){
-        
     }
     onSubmit(func){
         this.props.onsubmit = func;
@@ -26,4 +10,5 @@ export default class Form extends Component{
     onInput(func){
         this.props.oninput = func;
     }
+
 }
--- pages/home.js
+++ pages/home.js
@@ -1,18 +1,30 @@
+import Form from "../components/form.js";
+import Input from "../components/input.js";
 import { Page } from "../components/page.js";
 import { vNode, createNestedChild } from "../framework/engine.js";
 
-export default class HomePage extends Page{
-    constructor(){
+export default class HomePage extends Page {
+    constructor() {
         super()
     }
-
-    render(){
-        const div = vNode("div", { id: "container" })
-        const h1 = vNode("h1", { id: "test" }, "This is a Title")
-        const p = vNode("p", { id: "test" }, "This is a text cool, you need to be prepared to all bugs")
-        
-        const test = createNestedChild(div, h1, p)
-        
+    //renders the concerned page
+    render() {
+        // const div = vNode("div", { id: "container" })
+        // const h1 = vNode("h1", { id: "test" }, "This is a Form Title")
+        // const p = vNode("p", { id: "test" }, "This is a text cool, you need to be prepared to all bugs")
+        const form = new Form()
+        const input = new Input("text", "txt")
+        const password = new Input("password", "pass")
+        const button = new Input("submit", "submit")
+        button.props.value = "Submit"
+        form.onSubmit(e => { e.preventDefault(); getFormValues(e.target) })
+        const test = createNestedChild(form, input, password, button)
         return test
     }
+}
+
+const getFormValues = (form) => {
+    const data = new FormData(form)
+    const res = Object.fromEntries(data.entries())
+    console.log(res)
 }
