4fe206abd68cc15842ce29bc604e1c7d05b145db
gthenard <thenardduviviergaetan@gmail.com>
2024-04-16 11:59:01 +0200

    filer clearAll

--- components/list.js
+++ components/list.js
@@ -8,18 +8,49 @@ export default class List extends Component {
         super("ul", props)
         this.props.className = "list"
         this.domNode = render(this)
+        this.memoryChildren = [];
         // this.counter = new Counter({ id: "counter" }, this.children.length)
     }
-
     async update(task) {
         this.oldNode = this.domNode;
         const element = new ListElement(task, this);
         this.children.push(element);
+        this.memoryChildren.push(element);
         const patches = diff(this.oldNode, this);
         const rootNode = document.getElementById('list');
         await patch(rootNode, patches);
         this.domNode = render(this);
         // this.counter.updateCount(this.children.length)
+    }
+    async filter(etats) {
+        this.oldNode = this.domNode;
+        this.children = [];
+        if (etats !== "all") {
+            this.memoryChildren.forEach((element) => {
+                if (element.state() === etats) {
+                    this.children.push(element)
+                }
+            })
+        } else {
+            this.children = [...this.memoryChildren]
+        }
+        const patches = diff(this.oldNode, this);
+        const rootNode = document.getElementById('list');
+        await patch(rootNode, patches);
+        this.domNode = render(this);
+    }
+    async clearCompleted() {
+        this.oldNode = this.domNode;
+        this.children = [...this.memoryChildren]
+        this.memoryChildren.forEach((element) => {
+            if (element.state() === true) {
+                element.destroy();  
+            }
+        })
+        const patches = diff(this.oldNode, this);
+        const rootNode = document.getElementById('list');
+        await patch(rootNode, patches);
+        this.domNode = render(this);
     }
 }
 
@@ -31,6 +62,7 @@ class ListElement extends Component {
         // this.state = this.initState();
         this.domNode = render(this)
         this.parent = parent;
+        [this.state, this.setState] = useState(false)
     }
 
     // initState() {
@@ -43,18 +75,18 @@ class ListElement extends Component {
     // }
 
     render(content) {
-        const [state,setState] = useState(false)
+        // const [state,setState] = useState(false)
 
         const div = new Component("div", { className: "view" })
-        const input = new Component("input", { className: "toggle", type: "checkbox"})
+        const input = new Component("input", { className: "toggle", type: "checkbox" })
         const label = new Component("label", {}, [content])
         input.actionListener('click', async (e) => {
-            setState(!state());
+            this.setState(!this.state());
             const arr = this.props.className.split(' ');
-                state() ? arr.push('completed') : arr.pop()
-                input.props.checked = state();
-                this.props.className = arr.join(' ');
-                this.updateParent();
+            this.state() ? arr.push('completed') : arr.pop()
+            input.props.checked = this.state();
+            this.props.className = arr.join(' ');
+            this.updateParent();
         })
         const button = new Component("button", { className: "destroy" }, ["X"])
         button.actionListener('click', async (e) => { await this.destroy() })
@@ -67,6 +99,7 @@ class ListElement extends Component {
         const index = this.parent.children.indexOf(this);
         if (index > -1) {
             this.parent.children.splice(index, 1);
+            this.parent.memoryChildren.splice(index, 1);
             await this.updateParent();
         }
     }
