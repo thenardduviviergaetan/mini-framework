b25214c4b84a0fe93f320e4a4b921c0652c46a1c
GMZZ <legaynicolas76@gmail.com>
2024-04-16 08:21:28 +0200

    testing templating

--- app.js
+++ app.js
@@ -4,25 +4,24 @@ import Input from "./components/input.js"
 import List from "./components/list.js"
 import Form from "./components/form.js"
 import Link from "./components/link.js"
-import CustomForm from "./components/test/test.js"
 
-new CustomForm().createForm(input)
+// new CustomForm().createForm(input)
 //THIS COULD BE OUR REACT LIKE
 
 
-// const win = new Framework()
+const win = new Framework()
 
-// win.addComponent(vNode("h1", { id: "title" }, "TO:DO List"))
-// const list = new List({ id: "list" })
+win.addComponent(vNode("h1", { id: "title" }, "TO:DO List"))
+const list = new List({ id: "list" })
 
-// const input = new Input({ id: "input", placeholder: "Add your task here", name: "task"})
+const input = new Input({ id: "input", placeholder: "Add your task here", name: "task"})
 
-// const form = new Form({ id: "task-manager" }).createForm(input);
+const form = new Form({ id: "task-manager" }).createForm(input);
 
-// form.onSubmit((e) => {
-//     const task = getFormValues(e).task;
-//     list.update(task)
-// })
+form.onSubmit((e) => {
+    const task = getFormValues(e).task;
+    list.update(task)
+})
 
 // const list_items = ["Task 1", "Task 2", "Task 3"]
 
@@ -30,24 +29,24 @@ new CustomForm().createForm(input)
 //     list.addItem(item)
 // })
 
-// const box = vNode("div", {id: "box"})
+const box = vNode("div", {id: "box"})
 
-// const itemsLeft = vNode("span", { id: "items-left" }, "0 items left")
-// box.addElement(itemsLeft)
+const itemsLeft = vNode("span", { id: "items-left" }, "0 items left")
+box.addElement(itemsLeft)
 
-// const linkBox = vNode("div", {id: "link-box"})
-// const link = new Link("Go to test")
-// win.bindLink(link, "/test")
-// linkBox.addElement(link)
+const linkBox = vNode("div", {id: "link-box"})
+const link = new Link("Go to test")
+win.bindLink(link, "/test")
+linkBox.addElement(link)
 
-// const link2 = new Link("Go back")
-// win.bindLink(link2, "/")
-// linkBox.addElement(link2)
-// box.addElement(linkBox)
+const link2 = new Link("Go back")
+win.bindLink(link2, "/")
+linkBox.addElement(link2)
+box.addElement(linkBox)
 
-// const clear = new Link("Clear all")
-// box.addElement(clear)
+const clear = new Link("Clear all")
+box.addElement(clear)
 
-// win.addComponent(form)
-// win.addComponent(list)
-// win.addComponent(box)
+win.addComponent(form)
+win.addComponent(list)
+win.addComponent(box)
